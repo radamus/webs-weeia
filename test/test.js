913388aae67d179d1a64e3c9f474992395b0f785
Radek Adamus <adamus.r@gmail.com>
2014-05-03 08:40:20 +0200

    expres stub

--- test/test.js
+++ test/test.js
@@ -1,4 +1,26 @@
 var assert = require("assert");
 var service = require("../service");
 
-var serviceProviderStub;
+var ServiceProviderStub = function(){
+	this.all = function() {}
+	this.listen = function() {}
+}
+ServiceProviderStub.Request = function(){
+	this.renderView = function(){}
+	this.sendFile = function(){}
+	this.send = function(){}
+	this.download = function(){}
+}
+
+
+
+var testServiceProvider = function(){
+	return new ServiceProviderStub();
+}
+
+
+
+describe("service", function(){
+
+})
+
